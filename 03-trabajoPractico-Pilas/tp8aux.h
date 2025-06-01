43ee9b515943128848d9e5291ab5c023565e8212
Juanchumu <ricard9045@gmail.com>
2025-06-01 00:54:52 -0300

    punto8

---
+++ 03-trabajoPractico-Pilas/tp8aux.h
@@ -0,0 +1,8 @@
+Pila pila_buscar_repetidos(Pila pila);
+void intercambiar_pilas(Pila pila_1, Pila pila_2);
+bool pila_buscar(Pila pila, TipoElemento te_buscado);
+Pila pila_ordenar_asc(Pila pila);
+void p_mostrar_y_valor(Pila pila);
+int p_largo(Pila pila);
+
+
